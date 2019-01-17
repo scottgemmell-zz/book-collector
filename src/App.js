1f6c5aee203261a84b3f493b441aa7cb38bab2d2
Scott Gemmell <Scott_Gemmell@syntelinc.com>
2019-01-17 13:46:41 +0000

    Added /assets and some CSS changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { Button, Grid } from "react-bootstrap";
 import logo from "./logo.svg";
-import "./App.scss";
 import { Route, Link } from "react-router-dom";
 import BookAdd from "./components/BookAdd.jsx";
 import BookList from "./components/BookList.jsx";
@@ -18,11 +17,10 @@ class App extends Component {
 							Books
 						</Link>
 					</h1>
-					<div className="App-action">
-						<Button bsStyle="primary" href="/add/">
-							+ Add Book
-						</Button>
-					</div>
+					
+					<Button bsStyle="primary" href="/add/">
+						+ Add Book
+					</Button>
 				</header>
 				<main>
 					<Grid>
--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Route } from "react-router-dom";
 
 class Book extends Component {
 	// constructor(props){
