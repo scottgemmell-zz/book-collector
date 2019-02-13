4aea995fc97676fa61287d068bd1cf95a894f1d7
Scott Gemmell <Scott_Gemmell@syntelinc.com>
2019-02-13 10:58:52 +0000

    Removed FieldInput component

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,4 @@
 export { default as BookForm } from "./BookForm.jsx";
 export { default as BookList } from "./BookList.jsx";
 export { default as BookView } from "./BookView.jsx";
-export { default as FieldInput } from "./FieldInput.jsx";
 export { default as Page404 } from "./Page404.jsx";
