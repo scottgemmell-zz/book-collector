1ac6e0c37098e4ba21113996dad1a5784419268f
Scott Gemmell <Scott_Gemmell@syntelinc.com>
2019-02-13 16:03:03 +0000

    lint

--- src/helpers/validation.js
+++ src/helpers/validation.js
@@ -20,6 +20,6 @@ export const alphaNumeric = value =>
 		: undefined;
 
 export const minLength = min => value =>
-	value && value.length < min ? `Must be ${min} characters or more` : undefined
+	value && value.length < min ? `Must be ${min} characters or more` : undefined;
 
 export const minLength3 = minLength(3);
