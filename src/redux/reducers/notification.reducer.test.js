c149761bd2c20e6f8a6a15b877aa324f8a4a37a2
Scott Gemmell <Scott_Gemmell@syntelinc.com>
2019-02-15 16:09:55 +0000

    Additional tests for notifications

--- src/redux/reducers/notification.reducer.test.js
+++ src/redux/reducers/notification.reducer.test.js
@@ -1,6 +1,9 @@
 import { notificationReducer } from "./notification.reducer";
 
 describe("notificationReducer", () => {
+
+	const notification = [{ id: 101, message: "Failed to fetch" }];
+
 	it("returns default initialState of `undefined`", () => {
 		const newState = notificationReducer(undefined, { type: "SET_NOTIFICATION", payload: undefined, meta: "[Books]" });
 		expect(newState).toEqual([undefined]);
@@ -14,5 +17,20 @@ describe("notificationReducer", () => {
 	it("returns true when payload `true`", () => {
 		const newState = notificationReducer(undefined, { type: "[Books] SET_NOTIFICATION", payload: true, meta: "[Books]" });
 		expect(newState).toEqual([true]);
+	});
+
+	it("", () => {
+		const newState = notificationReducer(notification, { type: "[Books] REMOVE_NOTIFICATION", payload: 100, meta: "[Books]" });
+		expect(newState).toEqual(notification);
+	});
+
+	it("", () => {
+		const newState = notificationReducer(notification, { type: "[Books] REMOVE_NOTIFICATION", payload: 101, meta: "[Books]" });
+		expect(newState).toEqual([]);
+	});
+
+	it("Use default when non-matching type", () => {
+		const newState = notificationReducer(undefined, { type: "" });
+		expect(newState).toEqual([]);
 	});
 });
