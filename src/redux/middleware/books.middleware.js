29176370e40faf1b0de134fdcbaaddf840c4dfc0
Scott Gemmell <Scott_Gemmell@syntelinc.com>
2019-03-22 16:21:02 +0000

    prettier config

--- src/redux/middleware/books.middleware.js
+++ src/redux/middleware/books.middleware.js
@@ -1,5 +1,11 @@
 import history from "../../history";
-import { BOOKS, ADD_BOOK, DELETE_BOOK, FETCH_BOOKS, EDIT_BOOK } from "../constants";
+import {
+	BOOKS,
+	ADD_BOOK,
+	DELETE_BOOK,
+	FETCH_BOOKS,
+	EDIT_BOOK
+} from "../constants";
 import { setBooks } from "../actions/books.actions.js";
 import { API_ERROR, apiRequest, API_SUCCESS } from "../actions/api.actions";
 import { setLoader } from "../actions/ui.actions";
@@ -8,22 +14,21 @@ import { setNotification } from "../actions/notifications.actions";
 // @ToDo: View only with Normalization
 // https://www.googleapis.com/books/v1/volumes?q=react
 
-function getBookListUrl(id=""){
-	return `http://localhost:3001/books/${id}`; 
+function getBookListUrl(id = "") {
+	return `http://localhost:3001/books/${id}`;
 }
 
 export const booksMiddleware = () => next => action => {
 	next(action);
-	switch(action.type) {
-		
+	switch (action.type) {
 	case ADD_BOOK:
 		next(setLoader({ state: true, feature: BOOKS }));
 		next(
-			apiRequest({ 
+			apiRequest({
 				body: JSON.stringify(action.payload),
 				method: "POST",
-				url: getBookListUrl(), 
-				feature: BOOKS, 
+				url: getBookListUrl(),
+				feature: BOOKS
 			})
 		);
 		break;
@@ -31,49 +36,54 @@ export const booksMiddleware = () => next => action => {
 	case EDIT_BOOK:
 		next(setLoader({ state: true, feature: BOOKS }));
 		next(
-			apiRequest({ 
+			apiRequest({
 				body: JSON.stringify(action.payload),
-				method: "PUT", 
-				url: getBookListUrl(action.payload.id), 
-				feature: BOOKS, 
+				method: "PUT",
+				url: getBookListUrl(action.payload.id),
+				feature: BOOKS
 			})
 		);
 		break;
-	
+
 	case DELETE_BOOK:
 		next(setLoader({ state: true, feature: BOOKS }));
 		next(
-			apiRequest({ 
+			apiRequest({
 				body: null,
-				method: "DELETE", 
-				url: getBookListUrl(action.payload), 
-				feature: BOOKS, 
+				method: "DELETE",
+				url: getBookListUrl(action.payload),
+				feature: BOOKS
 			})
 		);
 		break;
-	
+
 	case FETCH_BOOKS:
 		next(setLoader({ state: true, feature: BOOKS }));
 		next(
-			apiRequest({ 
-				body: null, 
-				method: "GET", 
-				url: getBookListUrl(), 
-				feature: BOOKS,
+			apiRequest({
+				body: null,
+				method: "GET",
+				url: getBookListUrl(),
+				feature: BOOKS
 			})
 		);
 		break;
-	
+
 	case `${BOOKS} ${API_SUCCESS}`:
 		if (Array.isArray(action.payload) || action.payload === null) {
-			next(setBooks({ books: action.payload, normalizeKey: "id"}));
+			next(setBooks({ books: action.payload, normalizeKey: "id" }));
 		}
 		history.push("/");
 		next(setLoader({ state: false, feature: BOOKS }));
 		break;
 
 	case `${BOOKS} ${API_ERROR}`:
-		next(setNotification({ message: action.payload.message, feature: BOOKS }));
+		next(
+			setNotification({
+				message: action.payload.message,
+				feature: BOOKS
+			})
+		);
 		next(setLoader({ state: false, feature: BOOKS }));
 		break;
 
